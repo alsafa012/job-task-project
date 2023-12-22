7c5d8f9014d8cd4a3769592dde9c41c4e03845ab
alsafa012 <alsafa012@gmail.com>
2023-12-22 19:44:59 +0600

    live lnk added on readme file

--- src/Pages/HomePage/AllPersonsection.jsx
+++ src/Pages/HomePage/AllPersonsection.jsx
@@ -1,3 +1,4 @@
+import Container from "../../Shared/ContainerDiv/Container";
 import img1 from "../../assets/users/banker.jpeg";
 import img2 from "../../assets/users/banker.jpg";
 import img3 from "../../assets/users/developer (1).jpeg";
@@ -7,67 +8,88 @@ import img6 from "../../assets/users/profetional.webp";
 const AllPersonSection = () => {
      return (
           <div>
-               <h1 className="text-white text-center text-2xl md:text-5xl font-bold my-10">
-                    people are using this website
-               </h1>
-               <div className="my-5 grid md:grid-cols-3 gap-5">
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                         <figure>
-                              <img src={img1} alt="Shoes" />
-                         </figure>
-                         <div className="card-body">
-                              <h2 className="card-title">Shoes!</h2>
-                              <p>
-                                   Banker
-                              </p>
+               <Container>
+                    <h1 className="text-white text-center text-2xl md:text-5xl font-bold my-10">
+                         people are using this website
+                    </h1>
+                    <div className="my-5 grid md:grid-cols-3 gap-5">
+                         {/* 1 */}
+                         <div className="card bg-base-100 shadow-xl">
+                              {/* <figure> */}
+                              <img
+                                   src={img1}
+                                   alt="Shoes"
+                                   className="w-full object-fill h-[150px] rounded-t-xl"
+                              />
+                              {/* </figure> */}
+                              <div className="card-body">
+                                   <p className="text-xl font-bold text-center">
+                                        Banker
+                                   </p>
+                              </div>
+                         </div>
+                         {/* 2 */}
+                         <div className="card bg-base-100 shadow-xl">
+                              {/* <figure> */}
+                              <img
+                                   src={img2}
+                                   alt="Shoes"
+                                   className="w-full object-fill h-[150px] rounded-t-xl"
+                              />
+                              {/* </figure> */}
+                              <div className="card-body">
+                                   <p className="text-xl font-bold text-center">
+                                        Banker
+                                   </p>
+                              </div>
+                         </div>
+                         {/* 3 */}
+                         <div className="card bg-base-100 shadow-xl">
+                              {/* <figure> */}
+                              <img
+                                   src={img3}
+                                   alt="Shoes"
+                                   className="w-full object-fill h-[150px] rounded-t-xl"
+                              />
+                              {/* </figure> */}
+                              <div className="card-body">
+                                   <p className="text-xl font-bold text-center">
+                                        Developer
+                                   </p>
+                              </div>
+                         </div>
+                         {/* 4 */}
+                         <div className="card bg-base-100 shadow-xl">
+                              {/* <figure> */}
+                              <img
+                                   src={img4}
+                                   alt="Shoes"
+                                   className="w-full object-fill h-[150px] rounded-t-xl"
+                              />
+                              {/* </figure> */}
+                              <div className="card-body">
+                                   <p className="text-xl font-bold text-center">
+                                        Developer
+                                   </p>
+                              </div>
+                         </div>
+                         {/* 5 */}
+                         <div className="card bg-base-100 shadow-xl">
+                              {/* <figure> */}
+                              <img
+                                   src={img5}
+                                   alt="Shoes"
+                                   className="w-full object-fill h-[150px] rounded-t-xl"
+                              />
+                              {/* </figure> */}
+                              <div className="card-body">
+                                   <p className="text-xl font-bold text-center">
+                                        Professional
+                                   </p>
+                              </div>
                          </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                         <figure>
-                              <img src={img2} alt="Shoes" />
-                         </figure>
-                         <div className="card-body">
-                              <h2 className="card-title">Shoes!</h2>
-                              <p>
-                                   Banker
-                              </p>
-                         </div>
-                    </div>
-                    {/* 3 */}
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                         <figure>
-                              <img src={img3} alt="Shoes" />
-                         </figure>
-                         <div className="card-body">
-                              <h2 className="card-title">Shoes!</h2>
-                              <p>
-                                   Developer
-                              </p>
-                         </div>
-                    </div>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                         <figure>
-                              <img src={img4} alt="Shoes" />
-                         </figure>
-                         <div className="card-body">
-                              <h2 className="card-title">Shoes!</h2>
-                              <p>
-                                   Developer
-                              </p>
-                         </div>
-                    </div>
-                    <div className="card w-96 bg-base-100 shadow-xl">
-                         <figure>
-                              <img src={img5} alt="Shoes" />
-                         </figure>
-                         <div className="card-body">
-                              <h2 className="card-title">Shoes!</h2>
-                              <p>
-                                   Professional
-                              </p>
-                         </div>
-                    </div>
-               </div>
+               </Container>
           </div>
      );
 };
