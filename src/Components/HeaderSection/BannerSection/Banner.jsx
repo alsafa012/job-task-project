f731200e50ee8e20f3cbf56e8b485ac4ee9285e2
alsafa012 <alsafa012@gmail.com>
2023-12-22 19:49:26 +0600

    banner updated

--- src/Components/HeaderSection/BannerSection/Banner.jsx
+++ src/Components/HeaderSection/BannerSection/Banner.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
-import bannerImg from "../../../assets/Banner/task-management-tips.webp"
+// import bannerImg from "../../../assets/Banner/task-management-tips.webp"
 // import bannerImg from "../../../assets/Banner"
 // import bannerImg from "../../../assets/Banner"
 
@@ -21,14 +21,11 @@ const Banner = () => {
                     <div className="hero-overlay bg-opacity-60"></div>
                     <div className="hero-content text-center text-neutral-content">
                          <div className="max-w-md">
-                              <h1 className="mb-5 text-5xl font-bold">
-                                   Hello there
+                              <h1 className="mb-5 text-2xl md:text-5xl font-bold">
+                                   Welcome to Task Management Website
                               </h1>
-                              <p className="mb-5">
-                                   Provident cupiditate voluptatem et in.
-                                   Quaerat fugiat ut assumenda excepturi
-                                   exercitationem quasi. In deleniti eaque aut
-                                   repudiandae et a id nisi.
+                              <p className="mb-5 text-lg">
+                                  In here anyone can manage their task by add task on our dashboard.
                               </p>
                               <Link to="/dashboard/allTask">
                                    <button className="btn primary-btn hover:bg-[#252734]">
