4ca69842dde641bb36b936c2236e3291f6c06851
alsafa012 <alsafa012@gmail.com>
2023-12-22 19:15:23 +0600

    new section add

---
+++ src/Pages/HomePage/AllPersonsection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllPersonsection = () => {
+     return (
+          <div>
+               AllPersonsection
+          </div>
+     );
+};
+
+export default AllPersonsection;
